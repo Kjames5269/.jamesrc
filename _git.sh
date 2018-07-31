81c8319997e984e05ecea8a6230bfc2616d55a7f
Kjames5269 <kjames5269@gmail.com>
2018-07-31 16:03:48 -0700

    messing with git commit to add '-ma' to prepend the branch name to the commit

--- _git.sh
+++ _git.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 alias cb="hub rev-parse --abbrev-ref HEAD"
+alias cc="git rev-parse HEAD"
+
+COMMIT_PREPEND_TAG="-ma"
 
 # A git wrapper just so you can pass cb as current branch to any command
 # To get the auto completion scripts shamelessly steal them from the hub github using... TODO
@@ -17,6 +20,25 @@ gitWrap() {
 		fi
 
 		ARGS=(${@:1:$length} $currBranch)
+
+    elif [[ $1 == "commit" ]] && [[ $2 == "$COMMIT_PREPEND_TAG" ]]; then
+        length=$(($# - 1))
+
+        currBranch=$(cb | cut -f1,2 -d '-' )
+
+        msg=$(echo ${3})
+
+        if [ $? -ne 0 ]; then
+            return 1
+        fi
+
+        firstWord=$(echo ${msg} | cut -f1 -d ' ')
+
+        if [[ "$firstWord" != "$currBranch" ]]; then
+            ARGS=("${1}" "-m" "\"${currBranch} ${msg}\"")
+        else
+            RGS=(${@})
+        fi
 
 	else
 		ARGS=(${@})

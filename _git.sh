c02006295a0331229a89ad078799df3bb87389ef
Kjames5269 <kjames5269@gmail.com>
2018-08-27 17:21:16 -0700

    allowing git request and pub to use set references (instead of needing origin master)

--- _git.sh
+++ _git.sh
@@ -142,20 +142,16 @@ function gitWrap() {
     elif [[ ${ARGS[1]} == "request" ]]; then
         if [ $# -eq 1 ] && ! [ -z ${lGIT_URL} ]; then
             open ${lGIT_URL}
-        elif [ $# -gt 1 ]; then
+        else
             unset lGIT_URL
             getGitURL ${ARGS[@]}
             if [ -z ${lGIT_URL} ]; then
-                echoerr "git ${ARGS[@]} was not successful does the remote exist?"
+                echoerr "git ${ARGS[@]} was not able to obtain the current remote, does the remote exist?"
                 return $?
             fi
             open ${lGIT_URL}
-        else
-            echoerr "git $@ was not able to obtain the last remote, did you push on this terminal?"
-            return $?
         fi
         return 0
-
 	else
 		ARGS=(${@})
 	fi
@@ -192,18 +188,28 @@ function gitWrap() {
 function getGitURL() {
 
     if [ $# -lt 2 ]; then
-        return 1
-    fi
+        tracking=$(${whichGit} for-each-ref --format='%(upstream:short)' $(${whichGit} symbolic-ref -q HEAD)) 2> /dev/null
 
-    while [[ $2 =~ ^-.+ ]]; do
-        shift
-    done
+        if [ $? -ne 0 ]; then
+            return $?
+        fi
+
+        gitURLArgs[2]=$(echo ${tracking} | cut -f1 -d '/')
+        gitURLArgs[3]=$(echo ${tracking} | cut -f2 -d '/')
+        unset tracking
+
+    else
+        while [[ $2 =~ ^-.+ ]]; do
+            shift
+        done
+        gitURLArgs=($@)
+    fi
 
     if [[ $# -eq 3 ]]; then
-        lGIT_BRANCH="tree/$3"
+        lGIT_BRANCH="tree/${gitURLArgs[3]}"
     fi
 
-    lGIT_URL=$(${whichGit} config --get "remote.${2}.url")
+    lGIT_URL=$(${whichGit} config --get "remote.${gitURLArgs[2]}.url")
     # If the remote is not found
     if [[ ${lGIT_URL} == "" ]]; then
         return 1
@@ -217,6 +223,7 @@ function getGitURL() {
     fi
     lGIT_URL=${lGIT_URL}${lGIT_BRANCH}
     unset lGIT_BRANCH
+    unset gitURLArgs
 
 }
 function mvnFmt() {

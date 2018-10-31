1b1b083eaec12216a3737a994ffe108dfd674fb9
Kjames5269 <kjames5269@gmail.com>
2018-10-30 19:14:48 -0700

    gitCheckout is getting a new usecase. git checkout "user:branch" will now get to that branch. Only works with github as of now

--- git/_checkout.sh
+++ git/_checkout.sh
@@ -2,7 +2,7 @@
 
 function precheckoutHook() {
 
-    if [ $# -ne 2 ] || ! [[ $2 =~ ^:.* ]]; then
+    if [ $# -ne 2 ] || ! [[ $2 =~ : ]]; then
         return 0
     fi
 
@@ -43,7 +43,7 @@ function precheckoutHook() {
         else
             ARGS=($1 ${neighborBranch})
         fi
-    else
+    elif [[ $2 =~ "^:.+$" ]]; then
         baseBranch=$(${whichGit} branch | grep \* | cut -c3-)
         count=$(echo ${baseBranch} | tr -cd '-' | wc -c)
         if [ ${count} -le 1 ]; then
@@ -62,6 +62,40 @@ function precheckoutHook() {
         fi
 
         ARGS=("rebase" "--onto" "master" "${firstJiraCommit}")
+
+    elif [[ $2 =~ "^.+:.+$" ]]; then
+
+        DEBUG $0 "check out PR format"
+
+        user=$(echo $2 | cut -f1 -d ':')
+        branch=$(echo $2 | cut -f2 -d ':')
+
+        # Setup the argument since it won't change from here.
+        ARGS=("checkout" "${branch}")
+
+        # Get all the remotes looking for the first half of the message: The user
+        for i in $(${whichGit} remote -v | awk '{print $2}' | uniq); do
+            DEBUG trace $0 "Looping through remotes $i"
+
+            C getUserProjTuple $(echo "${i}")
+            iUser=$(echo ${userProjTuple} | cut -f1 -d '/')
+
+            if [[ ${user} == ${iUser} ]]; then
+                DEBUG $0 "Found ${user}"
+                return 0
+            fi
+        done
+
+        project=$(echo ${userProjTuple} | cut -f2 -d '/')
+
+        DEBUG $0 "Adding ${user} as a remote"
+
+        C ${whichGit} remote add ${user} "https://github.com/${user}/${project}"
+
+        DEBUG $0 "Fetching branches from ${user}"
+
+        ${whichGit} fetch ${user} &> /dev/null
+
     fi
 
 }
@@ -72,4 +106,6 @@ postcheckoutHook() {
     unset count
     unset neighborBranch
     unset extension
+    unset user iUser userProjTuple branch
+
 }
